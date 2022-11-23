9e1d62f1fd688225519287bb64c63d02a81bea78
Batuhan Kacmaz <batuhan.kacmaz1105@gmail.com>
2022-11-23 17:53:47 +0300

    fixing stack navigator

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,23 @@
 import {View, Text} from "react-native";
 import React from "react";
 import {NavigationContainer} from "@react-navigation/native";
-import {createNativeStackNavigator} from "@react-navigation/stack";
-import Home from "./screens/Home";
+import {createStackNavigator} from "@react-navigation/stack";
+import Simpsons from "./screens/Simpsons";
 import Details from "./screens/Details";
 import Create from "./screens/Create";
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 const SimpsonsStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Home" component={Simpsons} />
       <Stack.Screen name="Details" component={Details} />
-      <Stack.Screen name="Create" component={Create} />
+      <Stack.Screen
+        name="Create"
+        component={Create}
+        options={{title: "Add New Character"}}
+      />
     </Stack.Navigator>
   );
 };
